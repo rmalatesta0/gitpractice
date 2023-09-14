b65a61c356821c4493a1e61fa360afe9fe679ba3
Rocco Malatesta (Class of 2026) <261150rm@internal.salesianum.org>
2023-09-14 14:17:58 -0400

    Chap1

---
+++ chp1.py
@@ -0,0 +1,74 @@
+x = 5
+age = 5
+greeting = "hello"
+print(greeting)
+
+RainingOutside = False;
+
+if(1):
+    print("Bring an Umbrella")
+
+num = 5 #this is type int
+num2 = 5.0 #this is a float
+testVal = False #this is a boolean it's a True/False value (1=true 0=false)
+TestVal = True
+
+print(type(testVal))
+print(TestVal)
+
+#we can turn numbers into strings, vice versa
+#decimals(floats) to integers, vice versa
+
+print(type(str(bool(1))))
+
+#concatentation below (space be4 'this')
+print("hello" + " this is a test")
+
+# == is testing for equality
+# = is for assigning a variable
+
+print (len("bruh"))     #printing the length of a phrase
+
+testVar = "hello there"
+print (len(testVar))   #printing the length of a variable's phrase
+
+print(2 ** 3 ** 2)
+print((2 ** 3) ** 2)
+
+# ok
+print ("-------------------Lions v. Chiefs-------------------")
+
+football1 = "lions"
+football2 = "chiefs"
+scoreLions = 0
+scoreChiefs = 0
+TDEX = 7
+TD = 6
+FG = 3
+
+#avoid hardcoding values
+
+scoreLions += TDEX
+print(scoreLions, scoreChiefs)
+scoreChiefs += TDEX*2
+print(scoreLions, scoreChiefs)
+scoreLions += TDEX
+scoreChiefs += FG
+print(scoreLions, scoreChiefs)
+scoreLions += TDEX
+scoreChiefs += FG
+print(scoreLions, scoreChiefs)
+print("Detroit Lions Victory, 21 to 20")
+
+print("--------------------------------------------------------------------------------------")
+#ok
+
+name = input("Enter Your Name: ")
+print("Hello, ")
+print(name)
+year  = input("Enter the Year: ")
+print(year)
+
+print("---------------")
+
+#if git we have a repository locally on two different machines, and the local repository is behind the most up-to-date version of the online repository, I need to run "git pull" to update the local repository.
