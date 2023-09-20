556530344dd264d3b9c98d14d75a0d116a51c5ad
Rocco Malatesta (Class of 2026) <261150rm@internal.salesianum.org>
2023-09-20 09:51:10 -0400

    dice

--- part2.py
+++
@@ -1,1 +0,0 @@
-print("this is doc 2")
---
+++ testrandom.py
@@ -0,0 +1,10 @@
+import random
+
+prob = random.randrange(1,256)
+print(prob)
+prob = random.randrange(1,256)
+print(prob)
+prob = random.randrange(1,256)
+print(prob)
+print("☺‼")
+
